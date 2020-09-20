e45d42efaf1b25baf5d35c3e3dc79d0bbb5dcdc2
mccray austin <tubach75@hotmail.com>
2020-09-20 19:47:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join()
+  elsif array.size >= 3
+    array[-1].replace("and #{array[-1]}")
+    return array.join(", ")
+  elsif array.size == 2
+    return array.join(" and ")
+  end
 end
+oxford_comma(["fiddleheads","okra","kohlrabi"])
